6a407548bbc75c91df103bf233113eab361f6c3f
Diego618 <68928678+Diego618@users.noreply.github.com>
2026-03-06 18:51:05 +0000

    commit

--- main.c
+++ main.c
@@ -3,6 +3,16 @@
 
 
 int main(void) {
-    vector v;
-    vect_init(&v, 35);
+        printf("crocodile\n");
+    vector vect;
+    vect_init(&vect, 10);
+    for (int i = 0; i < 10; i++)
+    {
+        vect_push(&vect, i);
+    }
+    vect_print(&vect);
+    vect_push(&vect, 67);
+    vect_print(&vect);
+    vect_free(&vect);
+    return 0;
 }
--- vector/vector.c
+++ vector/vector.c
@@ -4,18 +4,6 @@
 
 int main(void) {
 
-    printf("crocodile\n");
-    vector vect;
-    vect_init(&vect, 10);
-    for (int i = 0; i < 10; i++)
-    {
-        vect_push(&vect, i);
-    }
-    vect_print(&vect);
-    vect_push(&vect, 67);
-    vect_print(&vect);
-    vect_free(&vect);
-    return 0;
 }
 
 
